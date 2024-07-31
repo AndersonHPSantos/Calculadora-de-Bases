12520306863c112926ce6a3f76a43a07b2804a95
Anderson Henrique <andersonhpolizzattos@gmail.com>
2024-07-30 22:07:32 -0300

    Protótipo de menu feito

--- CalculadoraDeBases.py
+++ CalculadoraDeBases.py
@@ -36,10 +36,39 @@ def conv2hex(resto):
         15: 'F'
     }
     return conversao.get(resto, str(resto))
-# Test
-valor = int(input("Digite o número: "))
-base = int(input("Digite a base para conversão: \n"
-                " [2]  Binário\n"
-                " [8]  Octal\n" + 
-                " [16] Hexadecimal\n"))
-print("({})10 = ({}){}" .format(valor, Dec2(valor, base), base))
+
+def despedida():
+    print("Obrigado por utilizar o programa!")
+# Menu
+print("Bem-vindo")
+op1 = int(input("Qual o tipo de conversão deseja realizar?\n"
+        "[1] Decimal para demais bases\n"
+        "[2] Demais bases para decimal\n"
+        "[0] Sair\n"))
+match op1:
+    case 0:
+        despedida()
+    case 1:
+        # Decimal para demais bases
+        resp = 'S'
+        while(resp.upper() == 'S'):
+            valor = int(input("Digite o número decimal: "))
+            base = int(input("Digite a base para conversão: \n"
+            "[2]  Binário\n"
+            "[8]  Octal\n" + 
+            "[16] Hexadecimal\n"))
+            if(base != 2 and base!= 8 and base != 16):
+                print("Opção inválida")
+            else:
+                print("({})10 = ({}){}" .format(valor, Dec2(valor, base), base))
+            # Chamada para repetição
+            resp = input("Deseja continuar(S/N)?: ")
+    case 2:
+        # demais bases para decimal
+        print("Em breve")
+    case _:
+        # Comando inválido
+        print("Opção inválida!")
+# Despedidas
+if(op1 != 0):
+    despedida()
